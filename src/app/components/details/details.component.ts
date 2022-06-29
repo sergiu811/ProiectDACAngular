8e4a1405d9278dfd82f8f7f7b12a36df2853ab7c
Idu Sergiu <idu_sergiu@yahoo.com>
2022-06-29 12:50:19 +0300

    Added the backend for getting the details for one movie

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Movie } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  movieRating = 0;
+  movieID!: string;
+  movie: Movie | undefined;
+  routeSub: Subscription = new Subscription;
+  movieSub: Subscription = new Subscription;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpService: HttpService
+  ) { }
 
   ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
+      this.movieID = params['id'];
+      this.getMovieDetails(this.movieID);
+    });
+  }
+
+  getMovieDetails(id: string): void {
+   this.httpService.getOne(id).subscribe((res:any) =>{
+    this.movie=res.result;
+   } )
+
   }
 
 }
